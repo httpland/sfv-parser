fed26ffe27c6d578dd187631b471ff893d3f0ec6
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-04-06 22:08:36 +0900

    feat(mod): export parser and related types

---
+++ mod.ts
@@ -0,0 +1,19 @@
+// Copyright 2023-latest the httpland authors. All rights reserved. MIT license.
+// This module is browser compatible.
+
+export { parseSfv } from "./parse.ts";
+export {
+  type BareItem,
+  Binary,
+  Boolean,
+  Decimal,
+  Dictionary,
+  InnerList,
+  Integer,
+  Item,
+  List,
+  Parameters,
+  type Sfv,
+  String,
+  Token,
+} from "./types.ts";

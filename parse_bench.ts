8bb1b24c09c325e1e951ae15cba4fd099300002e
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-04-09 00:26:02 +0900

    chore: fix typo

--- parse_bench.ts
+++ parse_bench.ts
@@ -3,13 +3,13 @@ import { parseSfv } from "./parse.ts";
 import largeGenerated from "https://cdn.jsdelivr.net/gh/httpwg/structured-field-tests/large-generated.json" assert {
   type: "json",
 };
-import * as v1 from "https://deno.land/x/sfv_parser@1.0.0-beta.3/mod.ts";
+import * as v1_0_0 from "https://deno.land/x/sfv_parser@1.0.0/mod.ts";
 
 const fieldValue = largeGenerated[0]?.raw.join("") ?? "";
 
 Deno.bench("latest", { baseline: true, group: "parse" }, () => {
-  parseSfv(fieldValue, "dictionary");
+  const a = parseSfv(fieldValue, "Dictionary");
 });
 Deno.bench("past", { group: "parse" }, () => {
-  v1.parseSfv(fieldValue, "dictionary");
+  const a = v1_0_0.parseSfv(fieldValue, "Dictionary");
 });
